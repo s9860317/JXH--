0820cab063a687f93a968e133e8b99f91c675b64
蒋旭晖 <s9860317@hotmail.com>
2018-01-11 00:25:32 +0800

    0110

--- JXH-网优工具/Form1.Designer.cs
+++ JXH-网优工具/Form1.Designer.cs
@@ -31,14 +31,16 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.xPanderPanelList1 = new BSE.Windows.Forms.XPanderPanelList();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.mapControl1 = new MapInfo.Windows.Controls.MapControl();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.openTablesStripButton1 = new MapInfo.Windows.Controls.OpenTablesStripButton();
-            this.mapControl1 = new MapInfo.Windows.Controls.MapControl();
+            this.xPanderPanelList1 = new BSE.Windows.Forms.XPanderPanelList();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             this.toolStrip1.SuspendLayout();
@@ -67,27 +69,6 @@
             this.tabPage1.Text = "GIS";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(732, 414);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // xPanderPanelList1
-            // 
-            this.xPanderPanelList1.CaptionStyle = BSE.Windows.Forms.CaptionStyle.Flat;
-            this.xPanderPanelList1.Dock = System.Windows.Forms.DockStyle.Left;
-            this.xPanderPanelList1.GradientBackground = System.Drawing.Color.Empty;
-            this.xPanderPanelList1.Location = new System.Drawing.Point(3, 3);
-            this.xPanderPanelList1.Name = "xPanderPanelList1";
-            this.xPanderPanelList1.Size = new System.Drawing.Size(260, 408);
-            this.xPanderPanelList1.TabIndex = 0;
-            this.xPanderPanelList1.Text = "xPanderPanelList1";
-            // 
             // splitContainer1
             // 
             this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
@@ -96,6 +77,10 @@
             this.splitContainer1.Name = "splitContainer1";
             this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.gMapControl1);
+            // 
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.mapControl1);
@@ -103,6 +88,19 @@
             this.splitContainer1.Size = new System.Drawing.Size(496, 408);
             this.splitContainer1.SplitterDistance = 206;
             this.splitContainer1.TabIndex = 1;
+            // 
+            // mapControl1
+            // 
+            this.mapControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mapControl1.IgnoreLostFocusEvent = false;
+            this.mapControl1.Location = new System.Drawing.Point(0, 25);
+            this.mapControl1.Name = "mapControl1";
+            this.mapControl1.Size = new System.Drawing.Size(494, 171);
+            this.mapControl1.TabIndex = 1;
+            this.mapControl1.Text = "mapControl1";
+            this.mapControl1.Tools.LeftButtonTool = null;
+            this.mapControl1.Tools.MiddleButtonTool = null;
+            this.mapControl1.Tools.RightButtonTool = null;
             // 
             // toolStrip1
             // 
@@ -118,22 +116,60 @@
             // 
             this.openTablesStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
             this.openTablesStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("openTablesStripButton1.Image")));
-            this.openTablesStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.openTablesStripButton1.ImageTransparentColor = System.Drawing.Color.White;
             this.openTablesStripButton1.MapControl = this.mapControl1;
             this.openTablesStripButton1.Name = "OpenTables";
             this.openTablesStripButton1.Size = new System.Drawing.Size(23, 22);
             this.openTablesStripButton1.Text = "Open Tables";
             this.openTablesStripButton1.UseDefaultImage = true;
             // 
-            // mapControl1
+            // xPanderPanelList1
             // 
-            this.mapControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.mapControl1.IgnoreLostFocusEvent = false;
-            this.mapControl1.Location = new System.Drawing.Point(0, 25);
-            this.mapControl1.Name = "mapControl1";
-            this.mapControl1.Size = new System.Drawing.Size(494, 171);
-            this.mapControl1.TabIndex = 1;
-            this.mapControl1.Text = "mapControl1";
+            this.xPanderPanelList1.CaptionStyle = BSE.Windows.Forms.CaptionStyle.Flat;
+            this.xPanderPanelList1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.xPanderPanelList1.GradientBackground = System.Drawing.Color.Empty;
+            this.xPanderPanelList1.Location = new System.Drawing.Point(3, 3);
+            this.xPanderPanelList1.Name = "xPanderPanelList1";
+            this.xPanderPanelList1.Size = new System.Drawing.Size(260, 408);
+            this.xPanderPanelList1.TabIndex = 0;
+            this.xPanderPanelList1.Text = "xPanderPanelList1";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(762, 414);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemmory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(0, 0);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 2;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomEnabled = true;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(494, 204);
+            this.gMapControl1.TabIndex = 0;
+            this.gMapControl1.Zoom = 0D;
             // 
             // Form1
             // 
@@ -145,6 +181,7 @@
             this.Text = "主界面";
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel2.ResumeLayout(false);
             this.splitContainer1.Panel2.PerformLayout();
             this.splitContainer1.ResumeLayout(false);
@@ -164,6 +201,7 @@
         private System.Windows.Forms.ToolStrip toolStrip1;
         private MapInfo.Windows.Controls.OpenTablesStripButton openTablesStripButton1;
         private MapInfo.Windows.Controls.MapControl mapControl1;
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
     }
 }
 
